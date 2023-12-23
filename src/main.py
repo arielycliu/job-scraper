f600e04fabe5e4ad66395c7c549b2adb7b72bf09
Ariel Liu <ariel.yc.liu@gmail.com>
2023-12-22 23:49:43 -0500

    Added tests for Indeed and CSV

--- src/main.py
+++
@@ -1,7 +0,0 @@
-from src.scrapper.indeed_scrapper import IndeedScrapper
-from csv_writer import write_to_csv
-
-indeed = IndeedScrapper("Python Intern", "Toronto")
-job_data = indeed.fetch_data()
-print(job_data)
-write_to_csv("job_data.csv", job_data)
---
+++ src/tests/test_all.py
@@ -0,0 +1,22 @@
+from src.scrapper.indeed_scrapper import IndeedScrapper
+from src.csv_writer import write_to_csv
+import csv
+
+def test_indeed():
+    indeed = IndeedScrapper("Python Intern", "Toronto")
+    job_data = indeed.fetch_data()
+    assert job_data != []
+
+def test_writing_csv():
+    indeed = IndeedScrapper("Python Intern", "Toronto")
+    job_data = indeed.fetch_data()
+    write_to_csv("job_data.csv", job_data)
+
+    with open("C:/Users/ariel/!Github/job-scrapper/src/csv_data/job_data.csv", "r") as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        assert next(reader) is not None
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main()
